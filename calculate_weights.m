cc12aeda588180b1872f2f4f0cc61b2ebfe09f38
Fernando Angulo Barba <nando@Fernandos-MacBook-Air.local>
2023-07-12 09:09:15 -0700

    Figured out why the output wasn't symmetric

--- calculate_weights.m
+++ calculate_weights.m
@@ -9,12 +9,19 @@ function W = calculate_weights(A, beta)
 [n,m] = size(A);
 MN = m*n;
 Aij = reshape(A', MN, 1);
+dmax = 0;
+for i=1:MN
+    for j=i:MN
+        pm = (Aij(i)-Aij(j)).^2;
+        if pm > dmax
+            dmax = pm;
+    end
+end
 W = eye(MN,MN);
 for i = 1:MN
    idx = connected(i,n,m);
    d=zeros(MN,1);
    d(idx) = (Aij(i) - Aij(idx)).^2;
-   dmax = max(d);
-    W(idx,i) = exp(-beta * d(idx) / dmax);
+   W(idx,i) = exp(-beta * d(idx) / dmax);
 end
 end
