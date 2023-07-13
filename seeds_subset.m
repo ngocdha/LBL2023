df003960c9e7b3368043d8e0eea3057b0b347f6f
Fernando Angulo Barba <nando@Fernandos-MacBook-Air.local>
2023-07-13 13:01:58 -0700

    Added output description

--- seeds_subset.m
+++ seeds_subset.m
@@ -4,6 +4,8 @@ function subset = seeds_subset(S, label)
     %      e.g. S(1,:) = [{x-index of pixel}, {y-index of pixel},
     %                      {label}]
     %   label: the desired label to retrieve seeds from
+    % Output:
+    %   subset: location of seeds corresponding to the given label
 
     idx = (S(:,3) == label);
     subset = S(idx, 1:2);
