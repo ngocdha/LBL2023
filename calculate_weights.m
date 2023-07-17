0432c36d348c6f585bc92fc64ee319ac4c822cc0
Fernando Angulo Barba <nando@Fernandos-MacBook-Air.local>
2023-07-17 11:54:45 -0700

    I think I fixed the symmetry (again; had a parenthesis in the wrong spot)

--- calculate_weights.m
+++ calculate_weights.m
@@ -4,12 +4,11 @@ function W = calculate_weights(A, beta)
 %   A: image
 %   beta: free parameter
 % Output:
-%   W: matrix of weights of pixel p_kl reaching pixel p_ij
+%   W: matrix of weights of pixels p_kl adjacent to pixel p_ij
 
 [n,m,z] = size(A);
 MN = m*n;
 W = zeros(MN,MN);
-% Aij = reshape(A', MN, 1);
 dmax = max(max(max(A)));
 A = [zeros(1,m+2,z);zeros(n,1,z), A, zeros(n,1,z); zeros(1,m+2,z)];
 % for i=1:MN
@@ -25,19 +24,19 @@ A = [zeros(1,m+2,z);zeros(n,1,z), A, zeros(n,1,z); zeros(1,m+2,z)];
 % end
 for i = 2:n+1
     for j = 2:m+1
-        k = 4*(i-2) + j-1;
+        k = m*(i-2) + (j-1);
         W(k,k) = 1;
-        if k-1 >= 1
-            W(k-1,k) = exp(-beta * sqrt(sum((A(i,j,:) - A(i,j-1,:).^2))) / dmax);
+        if k - 1 >= 1
+            W(k-1,k) = exp(-beta * sqrt(sum((A(i,j,:) - A(i,j-1,:)).^2)) / dmax);
         end
-        if k + 1 <= MN
-            W(k+1,k) = exp(-beta * sqrt(sum((A(i,j,:) - A(i,j+1,:).^2))) / dmax);
+        if k + 1 <= m
+            W(k+1,k) = exp(-beta * sqrt(sum((A(i,j,:) - A(i,j+1,:)).^2)) / dmax);
         end
-        if k-m >= 1
-            W(k-m,k) = exp(-beta * sqrt(sum((A(i,j,:) - A(i-1,j,:).^2))) / dmax);
+        if k - m >= 1
+            W(k-m,k) = exp(-beta * sqrt(sum((A(i,j,:) - A(i-1,j,:)).^2)) / dmax);
         end
         if k + m <= MN
-            W(k+m,k) = exp(-beta * sqrt(sum((A(i,j,:) - A(i+1,j,:).^2))) / dmax);
+            W(k+m,k) = exp(-beta * sqrt(sum((A(i,j,:) - A(i+1,j,:)).^2)) / dmax);
         end
     end
 end
