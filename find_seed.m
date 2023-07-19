4f3fc09a6a60969202b51093c53170954ab3aa73
Fernando Angulo Barba <nando@Fernandos-MacBook-Air.local>
2023-07-19 16:48:09 -0700

    updated to reflect LD being a vector, not a matrix

--- find_seed.m
+++ find_seed.m
@@ -1,16 +1,17 @@
-function k = find_seed(LD, i, j)
+function k = find_seed(LD, i, j, num_cols)
     % Input:
     %   LD: The limiting distribution of the walk
     %   (i,j): the position of ij-th pixel
     % Output:
     %   k: seed with highest probability of being measured
     %       at the ij-th pixel
-    l = size(LD, 3);
+    l = size(LD, 2);
+    idx = num_cols*(i-1) + j;
     max = -1;
     k = -1;
     for p = 1:l
-        if LD(i,j,p) > max
-            max = LD(i,j,p);
+        if LD(idx,p) > max
+            max = LD(idx,p);
             k = p;
         end
     end
