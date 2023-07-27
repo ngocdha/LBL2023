58f7b350a3342685db0f08fe237fba5d72a2c188
Fernando Angulo Barba <nando@Fernandos-MacBook-Air.local>
2023-07-27 15:09:12 -0700

    started quantum circuit for QRW. So far just have the increment and decrement gates

---
+++ first_QC.m
@@ -0,0 +1,39 @@
+nbQubits = 3;
+
+MCX = @qclab.qgates.MCX;
+H = @qclab.qgates.Hadamard;
+
+circuit = qclab.QCircuit( nbQubits ) ;
+circuit.push_back( H (nbQubits-1) ) ;
+increment( circuit ) ;
+
+decrement( circuit ) ;
+
+% QASM
+fID = 1;
+fprintf( fID, '\n\nQASM output:\n\n' );
+fprintf( fID, 'OPENQASM 2.0;\ninclude "qelib1.inc";\n\n');
+fprintf( fID, 'qreg q[%d];\n',nbQubits);
+circuit.toQASM( fID );
+
+% Draw circuit
+fprintf( fID, '\n\nCircuit diagram:\n\n' );
+circuit.draw( fID, 'S' );
+
+function increment(circuit)
+    MCX = @qclab.qgates.MCX;
+
+    n = double(circuit.nbQubits);
+    for i = 1:n-2
+        circuit.push_back( MCX(i:n-2, 0, ones(length(i:n-2))) );
+    end
+end
+
+function decrement(circuit)
+    MCX = @qclab.qgates.MCX;
+
+    n = double(circuit.nbQubits);
+    for i = 1:n-2
+        circuit.push_back( MCX(i:n-2, 0, zeros(length(i:n-2)))) ;
+    end
+end
