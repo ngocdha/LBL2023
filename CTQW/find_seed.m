51c182e9178f010530ee4c64a50a8ca039455fd2
Fernando Angulo Barba <nando@Fernandos-MacBook-Air.local>
2023-08-11 11:43:02 -0700

    renamed variable max to M. Leads to issues with max function otherwise

--- CTQW/find_seed.m
+++ CTQW/find_seed.m
@@ -7,11 +7,11 @@ function k = find_seed(LD, i, j, num_cols)
     %       at the ij-th pixel
     l = size(LD, 2);
     idx = num_cols*(i-1) + j;
-    max = -1;
+    M = -1;
     k = -1;
     for p = 1:l
-        if LD(idx,p) > max
-            max = LD(idx,p);
+        if LD(idx,p) > M
+            M = LD(idx,p);
             k = p;
         end
     end
